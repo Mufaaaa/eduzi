1b943489ee491b322a56d7c774ed1bd8a460a7dc
Mufaaaa <faiq22836@gmail.com>
2026-03-28 10:33:21 +0700

    update admin user

---
+++ laravel/app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms;
+use Illuminate\Support\Facades\Auth;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Forms\Components\TextInput::make('name')
+                    ->label('Nama Pengguna')
+                    ->required(),
+
+                Forms\Components\TextInput::make('email')
+                    ->label('Email')
+                    ->required(),
+            ]);
+    }
+}
---
+++ laravel/app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+use Filament\Tables;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama Pengguna')
+                    ->searchable(),
+                
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable(),
+                    
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
